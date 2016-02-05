ae247549108bc9e1d96d5a4afd17668faf76b98d
Sebastian Melchior <sebastian@melchior.me>
2016-02-05 15:51:12 +0100

    add ignore_startup_parameters param

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Christoph Krybus"
 maintainer_email "ckrybus@googlemail.com"
 license          "Apache 2.0"
 description      "Installs and configures pgbouncer"
-version          "0.1"
+version          "0.1.1"
 
 recipe "pgbouncer", "Installs and configures pgbouncer"
 
